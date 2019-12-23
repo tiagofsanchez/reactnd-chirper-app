1b2b15d6ee9cd8971ea181b9fca5edcc9ec1326f
Tiago Sanchez <tiagofsanchez@gmail.com>
2019-12-23 22:03:21 +0800

    FEATURE: redux store implemented

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './components/App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import reducer from "./reducers";
+import middleware from "./middleware";
+
+const store = createStore(reducer, middleware);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
